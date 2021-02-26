ae662570b91dd445f307c72de02d3fd13fdda817
Matthew Turner <turnermb92@gmail.com>
2021-02-26 17:03:09 -0600

    converted html and js to bitcoin project

--- js/app.js
+++ js/app.js
@@ -1,25 +1,37 @@
 // 404003-MusicPic-HKGVIEYD
-
+// Access-Control-Allow-Origin: https://tastedive.com
 let userInput, musicData;
 
-const $similarTo = $('#similarTo')
-const $results = $('results')
+const $cosHeader = $("#cosHeader");
+const $similarTo = $("#similarTo");
+const $results = $("#results");
+const $input = $('input[type="text"]');
 
-$('form').on('submit', getMusicData)
+// $("form").on("submit", getMusicData);
 
 const getMusicData = (data) => {
   event.preventDefault();
   userInput = $input.val();
+  console.log(userInput);
 
-  $.ajax()({
-  url: 'https://tastedive.com/api/similar?' + userInput + '&k=404003-MusicPic-HKGVIEYD'
+  $.ajax({
+    url:
+      "https://api.coinbase.com/v2/prices/BTC-" +
+      userInput.toUpperCase() +
+      "/buy",
   }).then(
-      (data) => {
-          musicData = data
-          displayResults()
-      }
-      (error) => {
-          console.log('Invalid Request', error)
-      }
-  )
+    (data) => {
+      pricingData = data;
+      displayResults();
+    },
+    (error) => {
+      console.log("Invalid Request", error);
+    }
+  );
+};
+
+$("form").on("submit", getMusicData);
+
+const displayResults = () => {
+  $similarTo.text(pricingData.data.amount + userInput.toUpperCase());
 };
